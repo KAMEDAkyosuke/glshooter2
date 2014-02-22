718142069ef9cd8817bc999d2a9bf1feccdd83e0
KAMEDAkyosuke <kameda-kyosuke@kayac.com>
2014-02-23 02:02:13 +0900

    quick and dirty work for the play station 3 controller support

--- src/main.js
+++ src/main.js
@@ -26,11 +26,75 @@ var DEBUG = true;
  */
 var VERSION = 99.9;
 
+function Gamepad(keymap){
+    this.keyboard = null;
+    this.keymap   = keymap;
+}
+Gamepad.prototype = {
+    _getGamepadKey : function(key){
+        var gamepads = navigator.webkitGetGamepads();
+        if(gamepads){
+            var gamepad = gamepads[1];    // 僕の環境だけ？
+            if(gamepad !== undefined){
+                var status = gamepad.buttons[this.keymap[key]] 
+                if(status !== undefined && status === 0){
+                    return false;
+                }
+                if(status !== undefined && status === 1){
+                    return true;
+                }
+            }
+        }
+        return false;
+    },
+    getKey : function(key){
+        if(this.keymap[key] === undefined){
+            console.log("getKey() not found " + key);
+            return this.keyboard.getKey(key);
+        }
+        return this._getGamepadKey(key);
+    },
+    getKeyDown : function(key){
+        if(this.keymap[key] === undefined){
+            console.log("getKeyDown() not found " + key);
+            return this.keyboard.getKeyDown(key);
+        }
+        return this._getGamepadKey(key);
+    }
+};
+
+function get_input(){
+    var gamepads = navigator.webkitGetGamepads()
+    if(! gamepads){
+        return null;
+    }
+    return new Gamepad({
+        // PS3 controller
+        "x"      : 0,    // bomb | hyper
+        "z"      : 1,    // full auto shot
+        "c"      : 2,    // shot | lazer
+        "escape" : 8,
+        "space"  : 9,
+        "up"     : 12,
+        "down"   : 13,
+        "left"   : 14,
+        "right"  : 15,
+    });
+}
+
 tm.preload(function() {
     if (STATS) tm.util.ScriptManager.loadStats();
 });
 tm.main(function() {
     gls2.GlShooter2("#canvas2d");
     if (STATS) gls2.core.enableStats();
+
+    var input = get_input();
+    if(input){
+        input.keyboard = gls2.core.keyboard;
+        gls2.core.keyboard = input;
+        gls2.core.keyboard._update     = input.keyboard._update;
+        gls2.core.keyboard.getKeyAngle = input.keyboard.getKeyAngle;
+    }
     gls2.core.run();
 });
